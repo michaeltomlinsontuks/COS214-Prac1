1a79709178960bc41142f52b4e3d7ff5d1f75b20
Wilmar-Smit <u24584216@tuks.co.za>
2025-08-07 18:40:39 +0200

    some memento privacy changes (i studied)

--- memento/Memento.h
+++ memento/Memento.h
@@ -4,20 +4,22 @@
 #include "../shape/Shape.h"
 #include "../testingFramework/array.h"
 
+class Canvas;
+
 class Memento
 {
-	friend string to_string(const Memento& );
+	friend string to_string(const Memento &);
+	friend void wilmarTesting();
+	friend Canvas;
 
 private:
 	Array<Shape> *shapes;
-
-public:
 	Memento(vector<Shape *> &elements);
 	Memento(Memento &copy);
 	// add to diagram
 	~Memento();
-	bool operator==(const Memento&);
+	bool operator==(const Memento &);
 };
-string to_string(const Memento&);
+string to_string(const Memento &);
 
 #endif
