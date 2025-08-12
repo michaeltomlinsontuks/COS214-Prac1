d1e13ba6792d68cce96a1b5f11c12fb949deb26a
michaeltomlinsontuks <159887992+michaeltomlinsontuks@users.noreply.github.com>
2025-08-12 11:53:48 +0200

    Final Demo

--- factory/ShapeFactory.h
+++ factory/ShapeFactory.h
@@ -4,7 +4,7 @@
 #include "../shape/Shape.h"
 
 class ShapeFactory {
-public:
+protected:
     virtual Shape* createShape(int length, int width, const std::string& colour, int position_x, int position_y) = 0;
     virtual std::string toString() = 0;
     virtual ~ShapeFactory() {}
